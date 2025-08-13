85c724211bc2c6e3b8a476bd899e2ac1a8222f39
Imoniyi Al-amin <topeimoniyi8@gmail.com>
2025-08-14 00:23:03 +0100

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,19 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=5b4fc8a8"; const _jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=63c366b3"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/src/App.tsx?t=1755126145265";
+import "/src/index.css?t=1755126145265";
+createRoot(document.getElementById("root")).render(/*#__PURE__*/ _jsxDEV(App, {
+    "data-lov-id": "src/main.tsx:5:52",
+    "data-lov-name": "App",
+    "data-component-path": "src/main.tsx",
+    "data-component-line": "5",
+    "data-component-file": "main.tsx",
+    "data-component-name": "App",
+    "data-component-content": "%7B%7D"
+}, void 0, false, {
+    fileName: "/dev-server/src/main.tsx",
+    lineNumber: 5,
+    columnNumber: 53
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IjtBQUFBLFNBQVNBLFVBQVUsUUFBUSxtQkFBaUI7QUFDNUMsT0FBT0MsU0FBUyxZQUFVO0FBQzFCLE9BQU8sY0FBWTtBQUVuQkQsV0FBV0UsU0FBU0MsY0FBYyxDQUFDLFNBQVVDLE1BQU0sZUFBQyxRQUFDSDtJQUFFSSxlQUFBO0lBQUFDLGlCQUFBO0lBQUFDLHVCQUFBO0lBQUFDLHVCQUFBO0lBQUFDLHVCQUFBO0lBQUFDLHVCQUFBO0lBQUFDLDBCQUFBIiwibmFtZXMiOlsiY3JlYXRlUm9vdCIsIkFwcCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiLCJkYXRhLWxvdi1pZCIsImRhdGEtbG92LW5hbWUiLCJkYXRhLWNvbXBvbmVudC1wYXRoIiwiZGF0YS1jb21wb25lbnQtbGluZSIsImRhdGEtY29tcG9uZW50LWZpbGUiLCJkYXRhLWNvbXBvbmVudC1uYW1lIiwiZGF0YS1jb21wb25lbnQtY29udGVudCJdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlcyI6WyJtYWluLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBjcmVhdGVSb290IH0gZnJvbSAncmVhY3QtZG9tL2NsaWVudCdcbmltcG9ydCBBcHAgZnJvbSAnLi9BcHAudHN4J1xuaW1wb3J0ICcuL2luZGV4LmNzcydcblxuY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZChcInJvb3RcIikhKS5yZW5kZXIoPEFwcCAvPik7XG4iXSwiZmlsZSI6Ii9kZXYtc2VydmVyL3NyYy9tYWluLnRzeCJ9
